35f614f624ab16313bec4b4e0cd232b9b05d535e
Jean Lima <jcoliveiralima19@gmail.com>
2026-05-12 14:03:40 -0300

    Adicionando VoteCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Container } from '@mui/material';
 import './App.css'
+import ResultCard from './components/assets/ResultCard';
+import VoteCard from './components/assets/VoteCard';
 
 function App() {
+  const voted = false;
+
   return (
-    <>
-    </>
+    <Container sx={{ height: "100dvh", display: "flex", flexDirection: "column", justifyContent: "center" }}>
+    {voted ? <ResultCard /> : <VoteCard />}
+    </Container>
   )
 }
 
--- src/components/assets/VoteCard/index.jsx
+++ src/components/assets/VoteCard/index.jsx
@@ -1,3 +1,82 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  FormLabel,
+  Paper,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
 export default function VoteCard() {
-  return <></>;
+  const [vote, setVote] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!vote) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+  };
+
+  return (
+    <Paper
+      elevation={4}
+      sx={{
+        maxWidth: 400,
+        margin: "0 auto",
+        padding: 4,
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        gap: 4,
+      }}
+    >
+      <Typography variant="h5">
+        Qual é sua linguagem de programação favorita?
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <FormControl>
+          <FormLabel>Escolha uma das opções abaixo:</FormLabel>
+          <RadioGroup
+            value={vote}
+            onChange={(e) => setVote(e.target.value)}
+            sx={{ marginTop: 3 }}
+          >
+            <FormControlLabel
+              value="JavaScript"
+              control={<Radio />}
+              label="JavaScript"
+            />
+            <FormControlLabel
+              value="Python"
+              control={<Radio />}
+              label="Python"
+            />
+            <FormControlLabel value="Java" control={<Radio />} label="Java" />
+            <FormControlLabel value="C#" control={<Radio />} label="C#" />
+            <FormControlLabel value="C++" control={<Radio />} label="C++" />
+          </RadioGroup>
+          {error && (
+            <FormHelperText>
+              Selecione uma opção antes de enviar.
+            </FormHelperText>
+          )}
+        </FormControl>
+        <Box sx={{ marginTop: 4 }}>
+          <Button type="submit" variant="contained">
+            Enviar
+          </Button>
+        </Box>
+      </form>
+    </Paper>
+  );
 }
